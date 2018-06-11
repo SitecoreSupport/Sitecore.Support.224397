ce40335616ff3f3113436a804f71f42893f59750
AntonKechashin <atk@sitecore.net>
2018-06-11 18:15:20 +0300

    Patch for 9.0.1

---
+++ src/Sitecore.Support.224397/Modules/EmailCampaign/Core/FieldValidators/EmailFieldValidator.cs
@@ -0,0 +1,62 @@
+﻿namespace Sitecore.Support.Modules.EmailCampaign.Core.FieldValidators
+{
+  using Sitecore.Configuration;
+  using Sitecore.Data.Validators;
+  using Sitecore.Diagnostics;
+  using Sitecore.Modules.EmailCampaign.Validators;
+  using System;
+  using System.Reflection;
+  using System.Runtime.Serialization;
+
+  [Serializable]
+  public class EmailFieldValidator : StandardValidator
+  {
+    // Fields
+    private RegexValidator _emailRegexValidator;
+
+    // Methods
+    public EmailFieldValidator()
+    {
+    }
+
+    public EmailFieldValidator(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    protected override ValidatorResult Evaluate()
+    {
+      string controlValidationValue = base.ControlValidationValue;
+      if (string.IsNullOrEmpty(controlValidationValue) || this.EmailRegexValidator.IsValid(controlValidationValue.Trim()))
+      {
+        return ValidatorResult.Valid;
+      }
+      string[] arguments = new string[] { controlValidationValue.Trim() };
+      base.Text = this.GetText("'{0}' is not a valid email address.", arguments);
+      return base.GetFailedResult(ValidatorResult.CriticalError);
+    }
+
+    protected override ValidatorResult GetMaxValidatorResult() =>
+      base.GetFailedResult(ValidatorResult.CriticalError);
+
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+      Assert.ArgumentNotNull(info, "info");
+      base.GetObjectData(info, context);
+      if (info.GetString("Name") == null)
+      {
+        info.AddValue("Name", this.Name);
+      }
+      else
+      {
+        info.GetType().GetMethod("UpdateValue", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(info, new object[] { "Name", this.Name, typeof(System.String) });
+      }
+    }
+
+    // Properties
+    public override string Name =>
+      "EmailFieldValidator";
+
+    private RegexValidator EmailRegexValidator =>
+      (this._emailRegexValidator ?? (this._emailRegexValidator = Factory.CreateObject("emailRegexValidator", true) as RegexValidator));
+  }
+}
---
+++ src/Sitecore.Support.224397/Modules/EmailCampaign/Core/FieldValidators/UrlFieldValidator.cs
@@ -0,0 +1,62 @@
+﻿namespace Sitecore.Support.Modules.EmailCampaign.Core.FieldValidators
+{
+  using Sitecore.Configuration;
+  using Sitecore.Data.Validators;
+  using Sitecore.Diagnostics;
+  using Sitecore.Modules.EmailCampaign.Validators;
+  using System;
+  using System.Reflection;
+  using System.Runtime.Serialization;
+
+  [Serializable]
+  public class UrlFieldValidator : StandardValidator
+  {
+    // Fields
+    private RegexValidator _urlRegexValidator;
+
+    // Methods
+    public UrlFieldValidator()
+    {
+    }
+
+    public UrlFieldValidator(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+
+    protected override ValidatorResult Evaluate()
+    {
+      string controlValidationValue = base.ControlValidationValue;
+      if (string.IsNullOrEmpty(controlValidationValue) || this.UrlRegexValidator.IsValid(controlValidationValue.Trim()))
+      {
+        return ValidatorResult.Valid;
+      }
+      string[] arguments = new string[] { controlValidationValue.Trim() };
+      base.Text = this.GetText("'{0}' is not a valid url.", arguments);
+      return base.GetFailedResult(ValidatorResult.CriticalError);
+    }
+
+    protected override ValidatorResult GetMaxValidatorResult() =>
+      base.GetFailedResult(ValidatorResult.CriticalError);
+
+    public override void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+      Assert.ArgumentNotNull(info, "info");
+      base.GetObjectData(info, context);
+      if (info.GetString("Name") == null)
+      {
+        info.AddValue("Name", this.Name);
+      }
+      else
+      {
+        info.GetType().GetMethod("UpdateValue", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(info, new object[] { "Name", this.Name, typeof(System.String) });
+      }
+    }
+
+    // Properties
+    public override string Name =>
+      "UrlFieldValidator";
+
+    private RegexValidator UrlRegexValidator =>
+      (this._urlRegexValidator ?? (this._urlRegexValidator = Factory.CreateObject("urlRegexValidator", true) as RegexValidator));
+  }
+}
